f3753f55452a6fd173adaa8be04cab73066fa717
Stephan de Goede <stephan.de.goede@gmail.com>
2023-01-01 20:17:24 +0100

    updated script

--- cali.py
+++ cali.py
@@ -1,6 +1,7 @@
 # Load required packages
 import streamlit as st 
 from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 
 # Load the dataset
 def california_housing():
@@ -9,4 +10,15 @@ def california_housing():
 if 'california_housing' not in st.session_state:
     california_housing()
 
+st.write('The detailed description of the data is listed below')
 st.write(st.session_state.california_housing.DESCR)
+
+st.subheader('Creating a Pandas Dataframe from the original dataset')
+st.table(st.session_state.california_housing.frame.head())
+
+st.write(st.session_state.california_housing.frame.columns)
+
+# creating test and training set
+X = st.session_state.california_housing.frame.loc[:, st.session_state.california_housing.frame.columns != 'MedHouseVal'].values
+y = st.session_state.california_housing.frame.loc[:, st.session_state.california_housing.frame.columns == 'MedHouseVal'].values
+x_train, x_test, y_train, y_test = train_test_split (X,y, test_size = 0.25, random_state=37)
