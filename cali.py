0aa2a2142b2ab8d3fe4ae97d2ee222c6bef744c5
Stephan de Goede <stephan.de.goede@gmail.com>
2023-01-23 13:51:32 +0100

    updated script and model.json

--- cali.py
+++ cali.py
@@ -37,18 +37,19 @@ x_train, x_test, y_train, y_test = train_test_split (X,y, test_size = 0.25, rand
 st.subheader('Setting the benchmark: Fit a simple linear regression- model')
 st.caption('Note that this model is previously fitted and loaded here, due to performance reasons')
 st.write('Below the source code of the linear model can be reviewed')
-st.code('''
-# creating test and training set
-X = st.session_state.california_housing.frame.loc[:, st.session_state.california_housing.frame.columns != 'MedHouseVal'].values
-y = st.session_state.california_housing.frame.loc[:, st.session_state.california_housing.frame.columns == 'MedHouseVal'].values
-x_train, x_test, y_train, y_test = train_test_split (X,y, test_size = 0.25, random_state=37)
+if st.button('click here to view source code',key=1):
+    st.code('''
+        # creating test and training set
+    X = st.session_state.california_housing.frame.loc[:, st.session_state.california_housing.frame.columns != 'MedHouseVal'].values
+    y = st.session_state.california_housing.frame.loc[:, st.session_state.california_housing.frame.columns == 'MedHouseVal'].values
+    x_train, x_test, y_train, y_test = train_test_split (X,y, test_size = 0.25, random_state=37)
 
-# linear model fit
-reg = LinearRegression().fit(x_train, y_train)
-reg.score(x_test,y_test)
+    # linear model fit
+    reg = LinearRegression().fit(x_train, y_train)
+    reg.score(x_test,y_test)
 
-# Save model to disk
-joblib.dump(reg, 'lin_reg.pkl')''')
+    # Save model to disk
+    joblib.dump(reg, 'lin_reg.pkl')''')
 
 def lin_reg():
     st.session_state.lin_reg = joblib.load('lin_reg.pkl')
@@ -70,33 +71,35 @@ st.write('RMSE of baseline model on test set:', round(st.session_state.rmse,2))
 # Building the dashboard on XGBOOST model:
 st.subheader('Model the California Housing Dataset using XGBOOST')
 st.write('Below the source code of the XGB model can be reviewed')
-st.code('''# Define the search space
-params = { 
-    # Learning rate shrinks the weights to make the boosting process more conservative
-    "learning_rate": [0.0001,0.001, 0.01, 0.1, 1] ,
-    # Maximum depth of the tree, increasing it increases the model complexity.
-    "max_depth": range(3,21,3),
-    # Gamma specifies the minimum loss reduction required to make a split.
-    "gamma": [i/10.0 for i in range(0,5)],
-    # Percentage of columns to be randomly samples for each tree.
-    "colsample_bytree": [i/10.0 for i in range(3,10)],
-    # reg_alpha provides l1 regularization to the weight, higher values result in more conservative models
-    "reg_alpha": [1e-5, 1e-2, 0.1, 1, 10, 100],
-    # reg_lambda provides l2 regularization to the weight, higher values result in more conservative models
-    "reg_lambda": [1e-5, 1e-2, 0.1, 1, 10, 100],
-    # the minimum number of samples that a node can represent in order to be split further.
-    "min_child_weight" : range(1,9,2)
-    }
-# Initiate the xgboost regressor (note GPU training is enabled)
-rs_model=RandomizedSearchCV(xgb_regressor,param_distributions=params,n_iter=param_comb, scoring="neg_root_mean_squared_error",n_jobs=None,cv=3,verbose=3,random_state=37) 
-# Perform a 3-fold cross-validated random-search of the grid for 18.900 possible combinations, evaluating ~25% of the total grid.
-param_comb = 18900
-# Actual fitting procedure:
-rs_model.fit(x_train,y_train)
-# Perform 10-fold cross validation on the best model. 
-print(np.mean(cross_val_score(rs_model.best_estimator_,x_test,y_test,cv=10)))
-# Save the best model
-rs_model.best_estimator_.save_model("xgboost_model.json)"''')
+if st.button('click here to view source code'):
+    st.code(''' # Define the search space
+    params = { 
+        # Learning rate shrinks the weights to make the boosting process more conservative
+        "learning_rate": [0.0001,0.001, 0.01, 0.1, 1] ,
+        # Maximum depth of the tree, increasing it increases the model complexity.
+        "max_depth": range(3,21,3),
+        # Gamma specifies the minimum loss reduction required to make a split.
+        "gamma": [i/10.0 for i in range(0,5)],
+        # Percentage of columns to be randomly samples for each tree.
+        "colsample_bytree": [i/10.0 for i in range(3,10)],
+        # reg_alpha provides l1 regularization to the weight, higher values result in more conservative models
+        "reg_alpha": [1e-5, 1e-2, 0.1, 1, 10, 100],
+        # reg_lambda provides l2 regularization to the weight, higher values result in more conservative models
+        "reg_lambda": [1e-5, 1e-2, 0.1, 1, 10, 100],
+        # the minimum number of samples that a node can represent in order to be split further.
+        "min_child_weight" : range(1,9,2)
+        }
+    # Initiate the xgboost regressor (note GPU training is enabled)
+    xgb_regressor = xgb.XGBRegressor(n_jobs=None,tree_method='gpu_hist',seed=37)
+    # Perform a 3-fold cross-validated random-search of the grid for 1000 possible combinations, evaluating just a fraction of the total grid.
+    param_comb = 1000
+    rs_model = RandomizedSearchCV(xgb_regressor,param_distributions=params,n_iter=param_comb,scoring="neg_root_mean_squared_error",n_jobs=None,cv=3,verbose=3,random_state=37) 
+    # Actual fitting procedure:
+    rs_model.fit(x_train,y_train)
+    # Perform 10-fold cross validation on the best model. 
+    print(np.mean(cross_val_score(rs_model.best_estimator_,x_test,y_test,cv=10)))
+    # Save the best model
+    rs_model.best_estimator_.save_model("xgboost_model.json),"''',)
 
 # Load the cross-validated tuned XGBOOST model
 def xgb_loaded():
@@ -136,10 +139,26 @@ plt.title ('xgboost.plot_importance(best XGBoost model) importance type = '+ str
 st.pyplot(importance_plot.figure)
 plt.clf()
 
+def shap_explainer():
+    st.session_state.explainer = shap.TreeExplainer(st.session_state.xbg_loaded)
+    st.session_state.shap_values = st.session_state.explainer.shap_values(x_train)
+
+
+
+
+if 'explainer' not in st.session_state:
+    shap_explainer()
+
+
 st.write('To handle this inconsitency, SHAP values give robust details, among which is feature importance')
-st.session_state.explainer = shap.TreeExplainer(st.session_state.xbg_loaded)
-st.session_state.shap_values = st.session_state.explainer.shap_values(x_train)
-plt.title('Assessing feature importance based on Shap values')
-st_shap(shap.summary_plot(st.session_state.shap_values,x_train,plot_type="bar",feature_names=st.session_state.california_housing.frame.columns, show=True))
-# st.pyplot(summary_plot.figure)
-# plt.clf()
+
+
+# st.session_state.explainer = shap.TreeExplainer(st.session_state.xbg_loaded)
+# st.session_state.shap_values = st.session_state.explainer.shap_values(x_train)
+# st.session_state.summary_plot_importance = shap.summary_plot(st.session_state.shap_values,x_train,plot_type="bar",feature_names=st.session_state.california_housing.frame.columns,show=False)
+# st.session_state.summary_plot = shap.summary_plot(st.session_state.shap_values,x_train,show=False,feature_names=st.session_state.california_housing.frame.columns) 
+st_shap(shap.summary_plot(st.session_state.shap_values,x_train,plot_type="bar",feature_names=st.session_state.california_housing.frame.columns))
+st.write('''SHAP values can also be used to represent the distribution of the training set of the respectable
+            SHAP value in relation with the Target value, in this case the median house value for California districts (MedHouseVal)''')
+st_shap(shap.summary_plot(st.session_state.shap_values,x_train,feature_names=st.session_state.california_housing.frame.columns))
+# summary_plot = st.session_state.summary_plot
